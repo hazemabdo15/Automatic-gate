772381a98eb2d36e969b7bbc3a82aed7a6871d12
Mariam1341 <76567004+Mariam1341@users.noreply.github.com>
2021-09-08 15:30:51 +0200

    commit

--- Automatic_gate/Automatic_gate.ino
+++ Automatic_gate/Automatic_gate.ino
@@ -1,3 +1,5 @@
+#include <Servo.h> 
+#define servo 3
 #define ledpin 13
 #define buzzerpin 11
 #define enterlrd A0
@@ -5,21 +7,30 @@
 int counter = 0;
 int enterflag = 0;
 int exitflag = 0;
+int Close = 0;
+int Open = 90;
+Servo ser; 
 void setup(){
  pinMode(ledpin,OUTPUT);
  pinMode(buzzerpin,OUTPUT);
  pinMode(enterlrd,INPUT);
  pinMode(exitlrd,INPUT);
  Serial.begin(9600);
+ ser.attach(servo); 
 }
 
 void loop(){
+   ser.write(Close);
    int enterstatus = analogRead(enterlrd);
    int exitstatus = analogRead(exitlrd);
    if(enterstatus < 20){
      enterflag =  1; }
    if(enterflag == 1){
       counter++;
+      if(counter < 5){
+      ser.write(Open);
+      delay(1000);
+      ser.write(Close);}
       Serial.println(counter);
       enterflag = 0;
      }
@@ -28,6 +39,10 @@ void loop(){
      exitflag = 1 ;}
      if(exitflag == 1){
       counter--;
+      if (counter >= 0 ){ 
+      ser.write(Open); //open the gate
+      delay(1000);
+      ser.write(Close);}
       if(counter < 0 ){
           counter = 0;  
    }
@@ -47,6 +62,5 @@ void loop(){
     delay(100);
    }
    }
-   
    delay(500);
 }
